340e9132af2526f425b25530b0c61a22cdce5b03
pasdar74 <mortazapasdar@gmail.com>
2026-07-07 18:35:39 +0500

    V 0.00.06

---
+++ public/api/hesabfa/config.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'apiKey' => 'EXRso1yN1rWAim2oR2BUhQWxc99lRpyj',
+    'userId' => 'mortazapasdar@gmail.com',
+    'password' => 'Mortaza74!',
+    'loginToken' => '347b7646ae73fc563fe8e4f07b62d92080d0c8aa3076dec6ca47dcd5f9c777c9bc4e47721a28b3e4e6aaa3126167138e',
+];
